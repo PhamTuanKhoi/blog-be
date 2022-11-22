845195f1a18c52c3a8c9d414ffb2b2d883198cf6
HO <nodejs@MacBook-Pro-cua-HO.local>
2022-11-22 18:16:28 +0700

    okay

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,7 +20,7 @@ class userController {
             expiresIn: "1h",
          });
 
-         res.status(200).json({ result: oldUser, token });
+         res.status(200).json({ user: oldUser, token });
       } catch (error) {
          res.status(500).json({ message: "Something went wrong" });
          console.log(error);
@@ -58,54 +58,3 @@ class userController {
 }
 
 module.exports = new userController();
-// module.exports = async function signinUser(req, res) {
-//    console.log("vo");
-//    const { username, password } = req.body;
-
-//    try {
-//       const oldUser = await userModel.findOne({ username });
-//       if (!oldUser) return res.status(404).json({ message: "User doesn't exist" });
-
-//       const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
-
-//       if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
-
-//       const token = jwt.sign({ username: oldUser.username, id: oldUser._id }, secret, {
-//          expiresIn: "1h",
-//       });
-
-//       res.status(200).json({ result: oldUser, token });
-//    } catch (error) {
-//       res.status(500).json({ message: "Something went wrong" });
-//       console.log(error);
-//    }
-// };
-
-// module.exports = async function signup(req, res) {
-//    const { username, password, name } = req.body;
-
-//    let date = new Date();
-//    let dob = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
-//    try {
-//       const oldUser = await userModel.findOne({ username });
-
-//       if (oldUser) {
-//          return res.status(400).json({ message: "User already exists" });
-//       }
-
-//       const hashedPassword = await bcrypt.hash(password, 12);
-
-//       const result = await userModel.create({
-//          username,
-//          password: hashedPassword,
-//          name,
-//          dob,
-//          created_at: date.getTime(),
-//       });
-
-//       res.status(201).json({ result });
-//    } catch (error) {
-//       res.status(500).json({ message: "Something went wrong" });
-//       console.log(error);
-//    }
-// };
